0d3d58184819bf8c93f843b8f9c6ebb209a1179d
Marc Penninga <marcpenninga@gmail.com>
2025-04-09 11:36:13 +0300

    Use callback for printing when processing all icons

--- texmf/tex/latex/fontawesome-free/fontawesome-free.lua
+++ texmf/tex/latex/fontawesome-free/fontawesome-free.lua
@@ -1,50 +1,62 @@
-local fontawesome_free = {
+local fonts = {
     solid   = "Font Awesome 6 Free-Solid-900.otf",
     regular = "Font Awesome 6 Free-Regular-400.otf",
     brands  = "Font Awesome 6 Brands-Regular-400.otf",
 }
 
-for fontname, fontfile in pairs(fontawesome_free) do
-    fontfile = kpse.find_file(fontfile, "opentype fonts")
+for fontname, filename in pairs(fonts) do
+    fontfile = kpse.find_file(filename, "opentype fonts")
     if fontfile then
         tex.sprint(string.format("\\font\\%s = \"[%s]\"", fontname, fontfile))
-        fontawesome_free[fontname] = fontfile
+        fonts[fontname] = fontfile
     else
-        fontawesome_free[fontname] = nil
+        tex.error(
+            string.format(
+                "[fontawesome-lua.sty] ERROR: font file '%s' not found!",
+                filename
+            )
+        )
     end
 end
 
-local fonts_version
-_, _, fonts_version = string.find(
-    fontloader.info(fontawesome_free.solid).version,
-    "%(Font Awesome version: ([%d.]+)%)"
-)
-fonts_version = fonts_version or "[\\emph{unknown}]"
+local fontawesome = {}
 
--- We cannot initialize the fa table at this point,
--- because TeX hasn't loaded the fonts yet;
--- hence we delay the initialization.
+-- We want to access the fonts both from TeX and from Lua.
+-- The easiest way to do that is to load the fonts in TeX
+-- (which we do above) and use font.id() from Lua.
+-- The downside of this approach is that TeX only loads
+-- the fonts when typesetting has started, not in the preamble;
+-- so we have to delay the Lua initialization until later.
 
-local fa = {}
 local is_initialized = false
-
 local function initialize()
-    for fontname, _ in pairs(fontawesome_free) do
-        fa[fontname] = font.getfont(font.id(fontname)).resources.unicodes
+    for fontname, _ in pairs(fonts) do
+        fontawesome[fontname] = font.getfont(font.id(fontname)).resources.unicodes
     end
 
     is_initialized = true
 end
 
 
-function print_icon(name)
+--[[------------------------------------------------------------------------
+    This function returns the named icon.
+
+    There is some cleverness in the implementation to hide the fact
+    that Font Awesome spreads its icons across three font files.
+    If `name` is present in the `brands` font, we return that icon;
+    if `name` ends with `-o`, we return the corresponding icon from
+    the `regular` font (which is in fact the `light` or `open` font);
+    otherwise we return the icon from the `solid` font (which is
+    actually the real `regular` font).
+    ------------------------------------------------------------------------]]
+local function icon(name)
     if not is_initialized then initialize() end
 
-    local b = fa.brands[name]
-    local s = fa.solid[name]
+    local b = fontawesome.brands[name]
+    local s = fontawesome.solid[name]
     local r, is_regular = string.gsub(name, "-o$", "", 1)
     if is_regular > 0 then
-        r = fa.regular[r]
+        r = fontawesome.regular[r]
     else
         r = nil
     end
@@ -64,8 +76,8 @@ function print_icon(name)
 end
 
 
---  Font Awesome contains a number of characters that aren't really "icons";
---  we exclude those from our lists of "all" icons.
+--  Font Awesome contains a number of characters that aren't really icons;
+--  we ignore these when processing "all" icons.
 local t = {
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e",
     "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
@@ -81,33 +93,50 @@ for _, name in pairs(t) do
     ignore[name] = true
 end
 
-function print_all_icons(...)
+
+--[[------------------------------------------------------------------------
+    This function processes all icons from one or more icon fonts.
+    For each icon, it calls a user-defined callback function
+    with two arguments: the name of the icon and the icon itself.
+
+    The varargs argument ... specifies the icon fonts to be processed;
+    this should be one or more of "solid", "regular" and "brands"
+    (i.e., the keys of the table `fonts` at the top of this file).
+    ------------------------------------------------------------------------]]
+local function process_all_icons(callback, ...)
     if not is_initialized then initialize() end
 
     local names, icons = {}, {}
-    for _, t in ipairs{...} do
-        for name, unicode in pairs(fa[t]) do
+    for _, font in ipairs{...} do
+        for name, unicode in pairs(fontawesome[font]) do
             if not (ignore[name] or string.match(name, "-sign$")) then
-                if t == "regular" then name = name .. "-o" end
+                if font == "regular" then name = name .. "-o" end
                 table.insert(names, name)
-                icons[name] = string.format("\\%s\\char%d", t, unicode)
+                icons[name] = string.format("\\%s\\char%d", font, unicode)
             end
         end
     end
 
     table.sort(names)
-    tex.sprint("\\begin{itemize}")
     for _, name in ipairs(names) do
-        tex.sprint(
-            string.format("\\item[%s] %s", icons[name], name))
+        callback(name, icons[name])
     end
-    tex.sprint("\\end{itemize}")
 end
 
 
+local fonts_version
+_, _, fonts_version = string.find(
+    fontloader.info(fonts.solid).version,
+    "%(Font Awesome version: ([%d.]+)%)"
+)
+fonts_version = fonts_version or "[\\emph{unknown}]"
+
+
 return {
-    print_icon = print_icon,
-    -- The next items are not part of the supported API and may change without warning!
-    _print_all_icons = print_all_icons,
+    icon = icon,
+
+    -- The following items are not part of the supported API
+    -- and may change without warning!
+    _process_all_icons = process_all_icons,
     _fonts_version = fonts_version,
 }
